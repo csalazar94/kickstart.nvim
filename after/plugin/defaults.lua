f8d8b6d0f729324a7558e298af3cf0533bd30c57
Camilo Salazar <csalazar@jumpitt.com>
2023-02-28 14:38:06 -0300

    add relative number and termguicolors

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,3 +1,9 @@
+-- relativenumber
+vim.opt.relativenumber = true
+
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
