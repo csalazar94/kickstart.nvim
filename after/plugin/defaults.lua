450b29da174c32f2d637624b00e818c6589dc2c7
Camilo Salazar <csalazar@jumpitt.com>
2023-02-28 14:44:00 -0300

    ✨ add telescope-gitmoji

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -7,3 +7,6 @@ vim.g.loaded_netrwPlugin = 1
 
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
+
+-- set gitmoji remap
+vim.keymap.set('n', '<leader>se', require('telescope').extensions.gitmoji.gitmoji, { desc = '[S]earch Git [E]moji' })
