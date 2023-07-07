eaf6c11e34457e713b0ff88850f81eb236f49b38
Camilo Salazar <camilosalazar94@gmail.com>
2023-07-07 07:58:10 -0400

    remove delve from dap

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -40,7 +40,6 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
         'node2',
       },
     }
