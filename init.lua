db2fb20293c363c444b5b8284c7e531675fd561a
Camilo Salazar <csalazar@jumpitt.com>
2023-11-01 12:41:08 -0300

    🔥 remove duplicated lines

--- init.lua
+++ init.lua
@@ -553,7 +553,3 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- vim: ts=2 sts=2 sw=2 et
--- vim: ts=2 sts=2 sw=2 et
--- vim: ts=2 sts=2 sw=2 et
--- vim: ts=2 sts=2 sw=2 et
